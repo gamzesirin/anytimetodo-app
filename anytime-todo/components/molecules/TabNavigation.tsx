4cb7554a5540037427f1c6060f350310d7bf8c0e
Gamze Şirin <gamze1sirin@gmail.com>
2024-03-28 20:10:14 +0300

    Component edit

--- anytime-todo/components/molecules/TabNavigation.tsx
+++ anytime-todo/components/molecules/TabNavigation.tsx
@@ -1,31 +1,33 @@
-import React from 'react';
+import React from 'react'
 
 interface TabNavigationProps {
-  activeTab: 'completed' | 'notCompleted';
-  setActiveTab: (tab: 'completed' | 'notCompleted') => void;
+	activeTab: 'completed' | 'notCompleted'
+	setActiveTab: (tab: 'completed' | 'notCompleted') => void
 }
 
 const TabNavigation: React.FC<TabNavigationProps> = ({ activeTab, setActiveTab }) => {
-  return (
-    <ul className="nav nav-tabs">
-      <li className="nav-item">
-        <button
-          className={`nav-link ${activeTab === 'notCompleted' ? 'active' : ''}`}
-          onClick={() => setActiveTab('notCompleted')}
-        >
-          Tamamlanmadı
-        </button>
-      </li>
-      <li className="nav-item">
-        <button
-          className={`nav-link ${activeTab === 'completed' ? 'active' : ''}`}
-          onClick={() => setActiveTab('completed')}
-        >
-          Tamamlandı
-        </button>
-      </li>
-    </ul>
-  );
-};
+	return (
+		<ul className="nav nav-tabs">
+			{/*tamamlanmamış görevler*/}
+			<li className="nav-item">
+				<button
+					className={`nav-link ${activeTab === 'notCompleted' ? 'active' : ''}`}
+					onClick={() => setActiveTab('notCompleted')}
+				>
+					Tamamlanmadı
+				</button>
+			</li>
+			{/*tamamlanmış görevler*/}
+			<li className="nav-item">
+				<button
+					className={`nav-link ${activeTab === 'completed' ? 'active' : ''}`}
+					onClick={() => setActiveTab('completed')}
+				>
+					Tamamlandı
+				</button>
+			</li>
+		</ul>
+	)
+}
 
-export default TabNavigation;
+export default TabNavigation
